f64d4b7b93a47490e7f5649560184f5e448c0742
mohabroha@mail.de <mohabroha@mail.de>
2026-08-21 16:06:12 +0200

    Add HTML email templates

--- accounts/functions.py
+++ accounts/functions.py
@@ -1,10 +1,12 @@
+from pathlib import Path
+
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
-from django.conf import settings
-from django.core.mail import send_mail
-from django.urls import reverse
 
 User = get_user_model()
 
@@ -34,6 +36,39 @@ def activate_user(uidb64, token):
     return True
 
 
+def _get_logo_path():
+    """Return the path to the Videoflix email logo."""
+
+    return Path(settings.BASE_DIR) / "accounts" / "templates" / "emails" / "Logo.svg"
+
+
+def _attach_logo(email):
+    """Attach the Videoflix logo as an inline email image."""
+
+    from email.mime.base import MIMEBase
+    from email import encoders
+
+    logo_path = _get_logo_path()
+
+    with logo_path.open("rb") as logo_file:
+        logo = MIMEBase("image", "svg+xml")
+        logo.set_payload(logo_file.read())
+
+    encoders.encode_base64(logo)
+
+    logo.add_header(
+        "Content-ID",
+        "<videoflix-logo>",
+    )
+    logo.add_header(
+        "Content-Disposition",
+        "inline",
+        filename="Logo.svg",
+    )
+
+    email.attach(logo)
+
+
 def send_activation_email(user, uidb64, token):
     """Send the account activation email to the provided user."""
 
@@ -42,35 +77,63 @@ def send_activation_email(user, uidb64, token):
         f"/pages/auth/activate.html?uid={uidb64}&token={token}"
     )
 
-    send_mail(
-        subject="Activate your Videoflix account",
-        message=(
-            "Welcome to Videoflix!\n\n"
-            "Please activate your account using the following link:\n"
-            f"{activation_url}\n\n"
-            "If you did not register for Videoflix, you can ignore this email."
-        ),
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        recipient_list=[user.email],
+    html_content = render_to_string(
+        "emails/confirm_email.html",
+        {
+            "user": user,
+            "activation_url": activation_url,
+        },
     )
+
+    text_content = (
+        "Welcome to Videoflix!\n\n"
+        "Please confirm your email address using the following link:\n"
+        f"{activation_url}\n\n"
+        "If you did not register for Videoflix, you can ignore this email."
+    )
+
+    email = EmailMultiAlternatives(
+        subject="Confirm your Videoflix email",
+        body=text_content,
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=[user.email],
+    )
+
+    email.attach_alternative(html_content, "text/html")
+    _attach_logo(email)
+    email.send()
 
 
 def send_password_reset_email(user, uidb64, token):
-    """Send a password reset email to the provided user."""
+    """Send the password reset email to the provided user."""
 
     reset_url = (
         f"{settings.FRONTEND_URL}"
         f"/pages/auth/confirm_password.html?uid={uidb64}&token={token}"
     )
 
-    send_mail(
-        subject="Reset your Videoflix password",
-        message=(
-            "You requested a password reset for your Videoflix account.\n\n"
-            "Please reset your password using the following link:\n"
-            f"{reset_url}\n\n"
-            "If you did not request a password reset, you can ignore this email."
-        ),
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        recipient_list=[user.email],
+    html_content = render_to_string(
+        "emails/password_reset.html",
+        {
+            "user": user,
+            "reset_url": reset_url,
+        },
     )
+
+    text_content = (
+        "You requested a password reset for your Videoflix account.\n\n"
+        "Please reset your password using the following link:\n"
+        f"{reset_url}\n\n"
+        "If you did not request a password reset, you can ignore this email."
+    )
+
+    email = EmailMultiAlternatives(
+        subject="Reset your Videoflix password",
+        body=text_content,
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=[user.email],
+    )
+
+    email.attach_alternative(html_content, "text/html")
+    _attach_logo(email)
+    email.send()
